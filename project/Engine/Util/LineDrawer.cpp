a4e80fde6e0987d9c20b6353f05b0bab3e201c2b
nakanuma <k023g0121@g.neec.ac.jp>
2025-04-22 17:23:51 +0900

    LineDrawerクラス作成途中

--- project/Engine/Util/LineDrawer.cpp
+++ project/Engine/Util/LineDrawer.cpp
@@ -1,5 +1,8 @@
 #include "LineDrawer.h"
 
+// Engine
+#include <Logger.h>
+
 LineDrawer* LineDrawer::GetInstance()
 {
     static LineDrawer instance;
@@ -8,7 +11,7 @@ LineDrawer* LineDrawer::GetInstance()
 
 void LineDrawer::Initialize()
 {
-    DirectXBase* dxBase = DirectXBase::GetInstance();
+    dxBase_ = DirectXBase::GetInstance();
 
     // 頂点バッファの作成
     D3D12_HEAP_PROPERTIES heapProperties = {};
@@ -26,7 +29,7 @@ void LineDrawer::Initialize()
     bufferDesc.SampleDesc.Count = 1;
     bufferDesc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
 
-    HRESULT hr = dxBase->GetDevice()->CreateCommittedResource(
+    HRESULT hr = dxBase_->GetDevice()->CreateCommittedResource(
         &heapProperties,
         D3D12_HEAP_FLAG_NONE,
         &bufferDesc,
@@ -38,14 +41,31 @@ void LineDrawer::Initialize()
     vertexBufferView.BufferLocation = vertexBuffer->GetGPUVirtualAddress();
     vertexBufferView.SizeInBytes = sizeof(Vertex) * 2;
     vertexBufferView.StrideInBytes = sizeof(Vertex);
+
+
+	// ルートシグネチャの作成
+	CreateRootSignature();
+	// InputLayoutの設定
+	SetInputLayout();
+	// DXC初期化
+	InitializeDXC();
+	// Shaderのコンパイル
+	ShaderCompile();
+	// RasterizerStateの設定
+	SetRasterizerState();
+	// 深度バッファ生成
+	CreateDepthBuffer();
+	// ブレンドステートの設定
+	SetBlendState();
+	// グラフィックスパイプラインの生成
+	CreateGraphicsPipeline();
 }
 
-void LineDrawer::DrawLine(const Float3& start, const Float3& end)
-{
+void LineDrawer::DrawLine(const Float3& start, const Float3& end, const Float4& color) {
     DirectXBase* dxBase = DirectXBase::GetInstance();
 
     // 頂点データを準備
-    Vertex vertices[] = { {start}, {end} };
+    Vertex vertices[] = { {start, color}, {end, color} };
 
     // GPUメモリへコピー
     void* mappedData = nullptr;
@@ -60,4 +80,121 @@ void LineDrawer::DrawLine(const Float3& start, const Float3& end)
 
     // 描画
     dxBase->GetCommandList()->DrawInstanced(2, 1, 0, 0);
+}
+
+void LineDrawer::CreateRootSignature() {
+	HRESULT result = S_FALSE;
+
+	// RootSignature作成
+	D3D12_ROOT_SIGNATURE_DESC descriptionRootSignature{};
+	descriptionRootSignature.Flags = D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT;
+
+	// DescriptorRange作成
+	D3D12_DESCRIPTOR_RANGE descriptorRange[1] = {};
+	descriptorRange[0].BaseShaderRegister = 0;                                                   // 0から始まる
+	descriptorRange[0].NumDescriptors = 1;                                                       // 数は1つ
+	descriptorRange[0].RangeType = D3D12_DESCRIPTOR_RANGE_TYPE_SRV;                              // SRVを使う
+	descriptorRange[0].OffsetInDescriptorsFromTableStart = D3D12_DESCRIPTOR_RANGE_OFFSET_APPEND; // Offsetを自動計算
+
+	// RootParameter作成。複数設定できるので配列
+	D3D12_ROOT_PARAMETER rootParameters[5] = {};
+	rootParameters[0].ParameterType = D3D12_ROOT_PARAMETER_TYPE_CBV;    // CBVを使う
+	rootParameters[0].ShaderVisibility = D3D12_SHADER_VISIBILITY_PIXEL; // PixelShaderで使う
+	rootParameters[0].Descriptor.ShaderRegister = 0;                    // レジスタ番号0とバインド
+
+	rootParameters[1].ParameterType = D3D12_ROOT_PARAMETER_TYPE_CBV;     // CBVを使う
+	rootParameters[1].ShaderVisibility = D3D12_SHADER_VISIBILITY_VERTEX; // VertexShaderで使う
+	rootParameters[1].Descriptor.ShaderRegister = 0;                     // レジスタ番号0を使う
+
+	rootParameters[2].ParameterType = D3D12_ROOT_PARAMETER_TYPE_DESCRIPTOR_TABLE;      // DescriptorTableを使う
+	rootParameters[2].ShaderVisibility = D3D12_SHADER_VISIBILITY_PIXEL;                // PixelShaderで使う
+	rootParameters[2].DescriptorTable.pDescriptorRanges = descriptorRange;             // Tableの中身の配列を指定
+	rootParameters[2].DescriptorTable.NumDescriptorRanges = _countof(descriptorRange); // Tableで利用する数
+
+	rootParameters[3].ParameterType = D3D12_ROOT_PARAMETER_TYPE_CBV;    // CBVを使う
+	rootParameters[3].ShaderVisibility = D3D12_SHADER_VISIBILITY_PIXEL; // PixelShaderで使う
+	rootParameters[3].Descriptor.ShaderRegister = 1;                    // レジスタ番号1を使う
+
+	rootParameters[4].ParameterType = D3D12_ROOT_PARAMETER_TYPE_CBV;
+	rootParameters[4].ShaderVisibility = D3D12_SHADER_VISIBILITY_PIXEL;
+	rootParameters[4].Descriptor.ShaderRegister = 2;
+
+	descriptionRootSignature.pParameters = rootParameters;             // ルートパラメータ配列へのポインタ
+	descriptionRootSignature.NumParameters = _countof(rootParameters); // 配列の長さ
+
+	// Samplerの設定
+	D3D12_STATIC_SAMPLER_DESC staticSamplers[1] = {};
+	staticSamplers[0].Filter = D3D12_FILTER_MIN_MAG_MIP_LINEAR;   // バイリニアフィルタ
+	staticSamplers[0].AddressU = D3D12_TEXTURE_ADDRESS_MODE_WRAP; // 0~1の範囲外をリピート
+	staticSamplers[0].AddressV = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
+	staticSamplers[0].AddressW = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
+	staticSamplers[0].ComparisonFunc = D3D12_COMPARISON_FUNC_NEVER;     // 比較しない
+	staticSamplers[0].MaxLOD = D3D12_FLOAT32_MAX;                       // ありったけのMipmapを使う
+	staticSamplers[0].ShaderRegister = 0;                               // レジスタ番号0を使う
+	staticSamplers[0].ShaderVisibility = D3D12_SHADER_VISIBILITY_PIXEL; // PixelShaderで使う
+	descriptionRootSignature.pStaticSamplers = staticSamplers;
+	descriptionRootSignature.NumStaticSamplers = _countof(staticSamplers);
+
+	// シリアライズしてバイナリにする
+	signatureBlob_ = nullptr;
+	errorBlob_ = nullptr;
+	result = D3D12SerializeRootSignature(&descriptionRootSignature, D3D_ROOT_SIGNATURE_VERSION_1, &signatureBlob_, &errorBlob_);
+	if (FAILED(result)) {
+		Log(reinterpret_cast<char*>(errorBlob_->GetBufferPointer()));
+		assert(false);
+	}
+	// バイナリを元に生成
+	rootSignature_ = nullptr;
+	result = dxBase_->GetDevice()->CreateRootSignature(0, signatureBlob_->GetBufferPointer(), signatureBlob_->GetBufferSize(), IID_PPV_ARGS(&rootSignature_));
+	assert(SUCCEEDED(result));
+}
+
+void LineDrawer::CreateGraphicsPipeline() {
+	HRESULT result = S_FALSE;
+
+	D3D12_GRAPHICS_PIPELINE_STATE_DESC graphicsPipelineStateDesc{};
+	graphicsPipelineStateDesc.pRootSignature = rootSignature_.Get();                                            // RootSignature
+	graphicsPipelineStateDesc.InputLayout = inputLayoutDesc_;                                                   // InputLayout
+	graphicsPipelineStateDesc.VS = {vertexShaderBlob_->GetBufferPointer(), vertexShaderBlob_->GetBufferSize()}; // VertexShader
+	graphicsPipelineStateDesc.PS = {pixelShaderBlob_->GetBufferPointer(), pixelShaderBlob_->GetBufferSize()};   // PixelShader
+	graphicsPipelineStateDesc.BlendState = blendDesc_;                                                          // BlendState
+	graphicsPipelineStateDesc.RasterizerState = rasterizerDesc_;                                                // RasterizerState
+	// 書き込むRTVの情報
+	graphicsPipelineStateDesc.NumRenderTargets = 1;
+	graphicsPipelineStateDesc.RTVFormats[0] = DXGI_FORMAT_R8G8B8A8_UNORM_SRGB;
+	// 利用するトポロジ（形状）のタイプ。三角形
+	graphicsPipelineStateDesc.PrimitiveTopologyType = D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE;
+	// どのように画面に色を打ち込むかの設定
+	graphicsPipelineStateDesc.SampleDesc.Count = 1;
+	graphicsPipelineStateDesc.SampleMask = D3D12_DEFAULT_SAMPLE_MASK;
+	// DepthStencilの設定
+	graphicsPipelineStateDesc.DepthStencilState = depthStencilDesc_;
+	graphicsPipelineStateDesc.DSVFormat = DXGI_FORMAT_D24_UNORM_S8_UINT;
+	// 実際に生成
+	graphicsPipelineState_ = nullptr;
+	result = dxBase_->GetDevice()->CreateGraphicsPipelineState(&graphicsPipelineStateDesc, IID_PPV_ARGS(&graphicsPipelineState_));
+	assert(SUCCEEDED(result));
+}
+
+void LineDrawer::SetInputLayout() {}
+
+void LineDrawer::InitializeDXC() {}
+
+void LineDrawer::ShaderCompile() {}
+
+D3D12_RASTERIZER_DESC LineDrawer::SetRasterizerState() { return D3D12_RASTERIZER_DESC(); }
+
+void LineDrawer::CreateDepthBuffer() {}
+
+D3D12_BLEND_DESC LineDrawer::SetBlendState() { 
+	blendDesc_.RenderTarget[0].RenderTargetWriteMask = D3D12_COLOR_WRITE_ENABLE_ALL;
+	blendDesc_.RenderTarget[0].BlendEnable = TRUE;
+	blendDesc_.RenderTarget[0].SrcBlend = D3D12_BLEND_SRC_ALPHA;
+	blendDesc_.RenderTarget[0].BlendOp = D3D12_BLEND_OP_ADD;
+	blendDesc_.RenderTarget[0].DestBlend = D3D12_BLEND_INV_SRC_ALPHA;
+	blendDesc_.RenderTarget[0].SrcBlendAlpha = D3D12_BLEND_ONE;
+	blendDesc_.RenderTarget[0].BlendOpAlpha = D3D12_BLEND_OP_ADD;
+	blendDesc_.RenderTarget[0].DestBlendAlpha = D3D12_BLEND_ZERO;
+
+	return blendDesc_;
 }
--- project/Engine/Util/LineDrawer.h
+++ project/Engine/Util/LineDrawer.h
@@ -9,13 +9,58 @@ class LineDrawer
 public:
 	static LineDrawer* GetInstance();
 	void Initialize();
-	void DrawLine(const Float3& start, const Float3& end);
+	void DrawLine(const Float3& start, const Float3& end, const Float4& color);
 
 private:
 	struct Vertex {
 		Float3 position;
+		Float4 color;
 	};
+
+	DirectXBase* dxBase_;
 
 	Microsoft::WRL::ComPtr<ID3D12Resource> vertexBuffer;
 	D3D12_VERTEX_BUFFER_VIEW vertexBufferView = {};
+
+private:
+	Microsoft::WRL::ComPtr<ID3D12RootSignature> rootSignature_;
+	Microsoft::WRL::ComPtr<ID3D12PipelineState> graphicsPipelineState_;
+
+	// ルートシグネチャの作成
+	void CreateRootSignature();
+	// グラフィックスパイプラインの生成
+	void CreateGraphicsPipeline();
+	// InputLayoutの設定
+	void SetInputLayout();
+	// DXC初期化
+	void InitializeDXC();
+	// Shaderのコンパイル
+	void ShaderCompile();
+	// RasterizerStateの設定
+	D3D12_RASTERIZER_DESC SetRasterizerState();
+	// 深度バッファ生成
+	void CreateDepthBuffer();
+	// BlendStateの設定
+	D3D12_BLEND_DESC SetBlendState();
+
+	Microsoft::WRL::ComPtr<ID3DBlob> signatureBlob_;
+	Microsoft::WRL::ComPtr<ID3DBlob> errorBlob_;
+
+	D3D12_INPUT_ELEMENT_DESC inputElementDescs_[3];
+	D3D12_INPUT_LAYOUT_DESC inputLayoutDesc_;
+
+	IDxcUtils* dxcUtils_;
+	IDxcCompiler3* dxcCompiler_;
+	IDxcIncludeHandler* includeHandler_;
+	IDxcBlob* vertexShaderBlob_;
+	IDxcBlob* pixelShaderBlob_;
+
+	D3D12_RASTERIZER_DESC rasterizerDesc_;
+
+	Microsoft::WRL::ComPtr<ID3D12Resource> depthStencilResource_;
+	D3D12_DEPTH_STENCIL_DESC depthStencilDesc_;
+	D3D12_DEPTH_STENCIL_VIEW_DESC dsvDesc_;
+	DescriptorHeap dsvDescriptorHeap_;
+
+	D3D12_BLEND_DESC blendDesc_; // kBlendModeNormal
 };
