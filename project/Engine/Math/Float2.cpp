73ee11927257da8aee8232e12a86f5b651ae1a3b
nakanuma <k023g0121@g.neec.ac.jp>
2025-05-30 21:13:07 +0900

    Float2クラスの更新

--- project/Engine/Math/Float2.cpp
+++ project/Engine/Math/Float2.cpp
@@ -1,6 +1,17 @@
 #include "Float2.h"
 
-Float2& Float2::operator+=(const Float2& other) {
+Float2 Float2::operator+(const Float2& other) const 
+{ 
+	return Float2(x + other.x, y + other.y); 
+}
+
+Float2 Float2::operator-(const Float2& other) const 
+{ 
+	return Float2(x - other.x, y - other.y); 
+}
+
+Float2& Float2::operator+=(const Float2& other) 
+{
 	this->x += other.x;
 	this->y += other.y;
 	return *this;
--- project/Engine/Math/Float2.h
+++ project/Engine/Math/Float2.h
@@ -5,6 +5,8 @@ struct Float2
 	float x;
 	float y;
 
+	Float2 operator+(const Float2& other) const;
+	Float2 operator-(const Float2& other) const;
 
 	Float2& operator+=(const Float2& other);
 };
